9affc73245ce4f8ab959d198424abbd1a3fe1f08
romax114 <70773814+romax114@users.noreply.github.com>
2020-10-03 18:00:42 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
 exports.min = function min (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
+  } else {
+  return Math.min.apply(null, array);
+}
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
+  } else {
+  return Math.max.apply(null, array);
+}
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
+  } else {
+  let sum = array.reduce(function(a,b) { return a + b;});
+  return sum/array.length;
+}
 }
